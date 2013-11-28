2661fc0646df1c9ce4734112c70f0c43ea4150c3
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-11-28 14:32:11 -0500

    Update ko_external_template_loader.js

--- ko_external_template_loader.js
+++ ko_external_template_loader.js
@@ -1,12 +1,47 @@
-/*var KOExternalTemplateLoader=function(_template_div,_url,_viewmodel){
-$('#'+_template_div).load(_url, {}, function() {
-    ko.applyBindings(_viewmodel);
-});
-}*/
-
-var KOExternalTemplateLoader=function(_template_div,_url,_viewmodel){
-$('#'+_template_div).load('/echo/html/', {
-    html: '<h3 data-bind="text: name"></h3><p>Credits: <span data-bind="text: credits"></span></p>'}, function() {
-    ko.applyBindings(_viewmodel);
-});
+/**
+ * A simple library to load knockout templates asynchronously and apply viewmodel to the templates.
+ */
+if (KOExternalTemplateLoader === undefined) {
+	var KOExternalTemplateLoader = {}
 }
+/**
+ * Change this variable to override the default option.
+ */
+KOExternalTemplateLoader.config={};
+/**
+ * Place to append the templates, e.g., if you want to inject the templates to a div, change this to '#divId'
+ */
+KOExternalTemplateLoader.config.templateHolder='body';
+/**
+ * Loads a template, when load is completed calls the _callback with the template html.
+ */
+KOExternalTemplateLoader.load=function(_templateUrl,_callback){
+	$.get(_templateUrl, function(_t) {
+		_callback(_t);
+	});
+};
+/**
+ * Loads a template, when load is completed, append the html to config.templateHolder and calls the _callback with the template html (if it is defined).
+ */
+KOExternalTemplateLoader.loadAndInject=function(_templateUrl,_callback){
+	KOExternalTemplateLoader.load(_templateUrl,function(_t){
+		$(KOExternalTemplateLoader.config.templateHolder).append(_t);
+		if(_callback!==undefined){
+			_callback(_t);
+		}
+	});
+};
+/**
+ * Loads a template, when load is completed, append the html to config.templateHolder, apply the knockout binding.
+ * If _viewModelScopeId is defined, apply the viewmodel to that id instead of this.
+ */
+KOExternalTemplateLoader.loadInjectAndApply=function(_templateUrl,_viewModel,_viewModelScopeId){
+	KOExternalTemplateLoader.loadAndInject(_templateUrl,function(_t){
+		if(_viewModelScopeId!==undefined){
+			ko.applyBindings(_viewModel,$('#'+_viewModelScopeId)[0]);
+		}
+		else{
+			ko.applyBindings(_viewModel);
+		}
+	});
+};
