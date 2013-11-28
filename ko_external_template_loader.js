765a005cc1af35dba07c7e08b0c25bdf09f5e624
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-11-28 12:06:32 -0500

    Update ko_external_template_loader.js

--- ko_external_template_loader.js
+++ ko_external_template_loader.js
@@ -1,5 +1,12 @@
-var KOExternalTemplateLoader=function(_template_div,_url,_viewmodel){
+/*var KOExternalTemplateLoader=function(_template_div,_url,_viewmodel){
 $('#'+_template_div).load(_url, {}, function() {
+    ko.applyBindings(_viewmodel);
+});
+}*/
+
+var KOExternalTemplateLoader=function(_template_div,_url,_viewmodel){
+$('#'+_template_div).load('/echo/html/', {
+    html: '<h3 data-bind="text: name"></h3><p>Credits: <span data-bind="text: credits"></span></p>'}, function() {
     ko.applyBindings(_viewmodel);
 });
 }
