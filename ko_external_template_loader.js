c7a663e23fda9ed0c5706c54c898e45fe447fdc4
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-12-10 12:04:04 -0500

    Update ko_external_template_loader.js

--- ko_external_template_loader.js
+++ ko_external_template_loader.js
@@ -1,21 +1,22 @@
 /**
  * A simple library to load knockout templates asynchronously and apply viewmodel to the templates.
+ * Depends on jquery and knockout.js
  */
-if (KOExternalTemplateLoader === undefined) {
-	var KOExternalTemplateLoader = {}
+if (KnockoutTemplateLoader === undefined) {
+	var KnockoutTemplateLoader = {}
 }
 /**
  * Change this variable to override the default option.
  */
-KOExternalTemplateLoader.config={};
+KnockoutTemplateLoader.config={};
 /**
  * Place to append the templates, e.g., if you want to inject the templates to a div, change this to '#divId'
  */
-KOExternalTemplateLoader.config.templateHolder='body';
+KnockoutTemplateLoader.config.templateHolder='body';
 /**
  * Loads a template, when load is completed calls the _callback with the template html.
  */
-KOExternalTemplateLoader.load=function(_templateUrl,_callback){
+KnockoutTemplateLoader.load=function(_templateUrl,_callback){
 	$.get(_templateUrl, function(_t) {
 		_callback(_t);
 	});
@@ -23,9 +24,9 @@ KOExternalTemplateLoader.load=function(_templateUrl,_callback){
 /**
  * Loads a template, when load is completed, append the html to config.templateHolder and calls the _callback with the template html (if it is defined).
  */
-KOExternalTemplateLoader.loadAndInject=function(_templateUrl,_callback){
-	KOExternalTemplateLoader.load(_templateUrl,function(_t){
-		$(KOExternalTemplateLoader.config.templateHolder).append(_t);
+KnockoutTemplateLoader.loadAndInject=function(_templateUrl,_callback){
+	KnockoutTemplateLoader.load(_templateUrl,function(_t){
+		$(KnockoutTemplateLoader.config.templateHolder).append(_t);
 		if(_callback!==undefined){
 			_callback(_t);
 		}
@@ -35,8 +36,8 @@ KOExternalTemplateLoader.loadAndInject=function(_templateUrl,_callback){
  * Loads a template, when load is completed, append the html to config.templateHolder, apply the knockout binding.
  * If _viewModelScopeId is defined, apply the viewmodel to that id instead of this.
  */
-KOExternalTemplateLoader.loadInjectAndApply=function(_templateUrl,_viewModel,_viewModelScopeId){
-	KOExternalTemplateLoader.loadAndInject(_templateUrl,function(_t){
+KnockoutTemplateLoader.loadInjectAndApply=function(_templateUrl,_viewModel,_viewModelScopeId){
+	KnockoutTemplateLoader.loadAndInject(_templateUrl,function(_t){
 		if(_viewModelScopeId!==undefined){
 			ko.applyBindings(_viewModel,$('#'+_viewModelScopeId)[0]);
 		}
